aac3d06213dbe1b01b9a05e04e8568ee06c3c2d3
mariano jimenez <marianojimenezperez1@gmail.com>
2024-04-03 19:50:42 +0200

    feat: home page working with dynamic data & localstorage persistance

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import PublicRoutes from './routes/PublicRoutes';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import './styles/index.scss';
 
 const queryClient = new QueryClient();
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <BrowserRouter>
         <PublicRoutes />
       </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
 );
