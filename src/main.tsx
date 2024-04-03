2c6a939a9339c0dfa3a634616cd1e1a3442ae44e
mariano jimenez <marianojimenezperez1@gmail.com>
2024-04-04 01:31:19 +0200

    fix: react query removed, unused

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import PublicRoutes from './routes/PublicRoutes';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import './styles/index.scss';
 
-const queryClient = new QueryClient();
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <PublicRoutes />
-    </BrowserRouter>
-    <ReactQueryDevtools initialIsOpen={false} />
-  </QueryClientProvider>
+  <BrowserRouter>
+    <PublicRoutes />
+  </BrowserRouter>
 );
